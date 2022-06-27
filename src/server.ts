d53d567fb9f8338bc74e2ac2eb19f422db898c25
ysknk <31568245+ysknk@users.noreply.github.com>
2022-06-27 17:06:50 +0900

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import sirv from "sirv";
+import sirv from "sirv"
 // NOTE: https://github.com/sveltejs/sapper/issues/1783#issuecomment-869221926
-// import polka from "polka";
-import compression from "compression";
-import * as sapper from "@sapper/server";
-import express from "express";
+// import polka from "polka"
+import compression from "compression"
+import * as sapper from "@sapper/server"
+import express from "express"
 
-const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === "development";
+const { PORT, NODE_ENV } = process.env
+const dev = NODE_ENV === "development"
 
 express()
   .use(
