73baa443c67d50da68cd845454e96f3ea6e07d6b
ysknk <31568245+ysknk@users.noreply.github.com>
2022-06-27 19:40:00 +0900

    Update index.svelte

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -23,8 +23,7 @@
   h1 Recent posts
 
   ul
-    | {#each posts as post}
-    // we're using the non-standard `rel=prefetch` attribute to tell Sapper to load the data for the page as soon as the user hovers over the link or taps it, instead of waiting for the 'click' event
-    li: a(rel="prefetch" href="blog/{post.slug}") {post.title}
-    | {/each}
+    +each('posts as post')
+      // we're using the non-standard `rel=prefetch` attribute to tell Sapper to load the data for the page as soon as the user hovers over the link or taps it, instead of waiting for the 'click' event
+      li: a(rel="prefetch" href="blog/{post.slug}") {post.title}
 </template>
