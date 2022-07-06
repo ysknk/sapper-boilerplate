77fb8b06ced0f53a34f0e7e7126870033cb22385
ysknk <31568245+ysknk@users.noreply.github.com>
2022-07-06 20:11:56 +0900

    Update Image.svelte

--- src/components/atoms/Image.svelte
+++ src/components/atoms/Image.svelte
@@ -10,14 +10,25 @@
 </script>
 
 <style lang="stylus">
-  .c-img
+  @import '../../styles/config'
+
+  .c-image
+    relative top 0 left 0
+    height 0
     :global(.wrapper)
       min-height 0 !important
     :global(img.main)
       height 100%
+
+    &__inner
+      absolute top 0 left 0
+      display block
+      width 100%
+      height 100%
 </style>
 
 <template lang="pug">
-  span.c-img
-    Image(src="{src}" ratio="{ratio}" width="{width}" height="{height}" alt="{alt}")
+  span.c-image(style!="padding-top:{ratio};")
+    span.c-image__inner
+      Image(src="{src}" ratio="{ratio}" width="{width}" height="{height}" alt="{alt}")
 </template>
