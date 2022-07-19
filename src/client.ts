1f240f757e1f23344bf755c15c3c1ee6a7e5c2d5
ysknk <31568245+ysknk@users.noreply.github.com>
2022-07-19 17:43:40 +0900

    Update client.ts

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,9 @@
 import * as sapper from '@sapper/app'
+import { rootpath } from './client.config'
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register(`${rootpath}/service-worker.js`)
+}
 
 sapper.start({
   target: document.querySelector('#wrapper')
